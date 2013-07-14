8141e2d403f05f277bd0a7451ba50acab36b6058
Ubuntu <ubuntu@ip-10-37-165-18.eu-west-1.compute.internal>
2013-07-14 09:27:12 +0000

    dd

--- web.js
+++ web.js
@@ -1,12 +1,28 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  
+	fs.readFileSync('index.html', function(err, data) {
+		if(err) return send404(response);	
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write(data, 'utf8');
+		res.end();
+	});		
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+send404 = function(res) {
+        res.writeHead(404);
+	res.write('404');
+	res.end();
+};
