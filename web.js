d75e659ad6d92ac5496bb213c271af3b69f2ea3f
Ubuntu <ubuntu@ip-10-37-165-18.eu-west-1.compute.internal>
2013-07-14 10:01:40 +0000

    from index

--- web.js
+++ web.js
@@ -7,15 +7,14 @@ var fs = require('fs');
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
   
-	fs.readFileSync('index.html', function(err, data) {
-		if(err) return send404(response);	
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write(data.toString('utf-8'), 'utf8');
-		res.end();
-	});		
+	var file = fs.readFileSync('index.html');	
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write(file.toString('utf-8'));
+	res.end();
+		
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
